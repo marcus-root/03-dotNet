b662161959878fc6a9e12cd360f2b6733b25f218
Marcus <github.com-public@0n0.de>
2026-04-14 18:43:59 +0200

    Aufgabenstellungen hinzugefügt

---
+++ 13 Tupel - 01 Grundlagen/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace dotNet_13_Tupel_01_Grundlagen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
